910838f94f276fa34b270c5b96844588fdd74d15
Noa Shay <noa224011224011@gmail.com>
2020-03-22 15:45:27 +0200

    commit for yuval

--- backend/index.js
+++ backend/index.js
@@ -11,17 +11,6 @@ app.use("/api/v1/labs", labsRouter);
 app.use(function(req, res, next) {
     next(createError(404));
   });
-  
-  // error handler
-  app.use(function(err, req, res, next) {
-    // set locals, only providing error in development
-    res.locals.message = err.message;
-    res.locals.error = req.app.get('env') === 'development' ? err : {};
-  
-    // render the error page
-    res.status(err.status || 500);
-    res.render('error');
-  });
 
 const port = process.env.PORT || 8000;
 app.listen(port, () => console.log(`Listening on port ${port}`));
--- backend/routes/labs.js
+++ backend/routes/labs.js
@@ -1,17 +1,18 @@
 var express = require("express");
 var router = express.Router();
-var {labs} = require("/IMPORTANT/Corona Project/Code/backend/data/labs");
+var { labs } = require("/IMPORTANT/Corona Project/Code/backend/data/labs");
 
 router.get("/", function(req, res, next) {
   res.send(labs);
 });
 
-// router.get("/:name", function(req, res, next) {
-//   labName = labs.find(lab => lab.name == req.params.name);
-//   if (labName === undefined) {
-//     res.sendStatus(404).send("name not found!");
-//   }
-//   res.send(labName);
-// });
+router.get("/:name", function(req, res, next) {
+  labName = labs.find(lab => lab.name == req.params.name);
+  if (labName === undefined) {
+    console.log(labName);
+    res.sendStatus(404).send("name not found!");
+  }
+  res.send(labName);
+});
 
 module.exports = router;
