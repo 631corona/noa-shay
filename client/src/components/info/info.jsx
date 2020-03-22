fbbdd8b7571a239cabb73bb830e9c7c0edd8cc39
Noa Shay <noa224011224011@gmail.com>
2020-03-22 03:53:05 +0200

    added new classes info and info-cars

--- client/src/components/info/info.jsx
+++ client/src/components/info/info.jsx
@@ -1,17 +1,24 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
 class Info extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {  }
-    }
-    render() { 
-        return ( 
-            <div>
-
-            </div>
-         );
-    }
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+  render() {
+    return (
+      <div>
+        <div>
+          <h1>Info</h1>
+          <h3>some information about the soliders and the bidud </h3>
+        </div>
+        <div>
+            
+        </div>
+      </div>
+    );
+  }
 }
- 
-export default Info;i
+
+export default Info;
+i;
